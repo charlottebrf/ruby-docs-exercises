3d0d45fcf21c886ba6b1656a35bb517e2cc86e2f
Charlotte <charlotte@Charlottes-MacBook-Air.local>
2016-12-23 15:14:57 +0000

    adds The Ruby Philosophy as first step of Head First Ruby

---
+++ ruby_philosophy.rb
@@ -0,0 +1,12 @@
+print Yukihirio Matsumoto
+#developed Ruby in 1990s in Japan
+print easy to learn & use
+print flexible enough to handle any programming task
+print let programmer focus on problem trying to solve
+print give programmer less stress
+print object orientated
+
+#the aims of ruby & some of it's benefits as a language
+#made public in 1995, was then added to by ruby community
+
+print flexibility , creativity, productivity 

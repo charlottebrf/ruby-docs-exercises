ac1b6447c46872d8790786da7aebffd4f5dc3560
Charlotte <charlotte@Charlottes-MacBook-Air.local>
2016-12-23 14:52:30 +0000

    adds koans on nil

---
+++ ruby_koans2.rb
@@ -0,0 +1,8 @@
+class AboutNil < Neo::Koan
+  def test_nil_is_an_object
+    assert_equal true, nil.is_a?(Object), "Unlike NULL in other languages"
+  end
+
+  #nil is an object, in ruby every value is an object it is an example of NilClass: 1
+  #unklike null in JS for example which has no value, is undefined & not an object
+  

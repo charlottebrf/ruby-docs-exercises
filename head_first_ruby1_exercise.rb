c63c1b7ca3868f85329080b3b10e1d58b2d03123
Charlotte <charlotte@Charlottes-MacBook-Air.local>
2016-12-23 16:32:45 +0000

    adds first set of exercises for chapter 1

---
+++ head_first_ruby1_exercise.rb
@@ -0,0 +1,43 @@
+42 / 6
+#7
+
+name = "Zaphod"
+#"Zaphod" assigns variable value
+
+name.upcase
+#"ZAPHOD" -you can call methods on an object stored in a variable
+
+name.reverse
+#goes back to original variable, so it will be lowercase
+#"dohpaZ"
+
+name.upcase.reverse
+#"DOHPAZ"
+
+name.class
+#String - will give the type of class
+
+
+name * 3
+#"ZaphodZaphodZaphod"
+
+5 > 4
+#true
+
+number = -32
+#-32 assigns variable value
+
+number.abs
+#32
+
+-32.abs
+#32
+
+number += 10
+#-22 -again it amends the original variable
+
+rand(25)
+#23 - this is a method call for a random number
+
+number.class
+#fixnum -a fixnum is a kind of integer

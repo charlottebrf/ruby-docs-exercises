7351f6832dddb9050f969cb71686aaeccc01688a
Charlotte <charlotte@Charlottes-MacBook-Air.local>
2016-12-24 19:04:18 +0000

    adds requirement comments for the game

---
+++ head_first_ruby1_game.rb
@@ -0,0 +1,22 @@
+#this game has 8 requirements:
+
+#1. Prompts the player to enter their name. Use their name to print a greeting.
+
+#2.Generate a random number from 1 - 100, & store as a target number for player to guess
+
+#3. Track how many guesses the player has made. Before each guess let them know how
+#many guesses out of 10 they have left.
+
+#4.Prompt player to make guess of what target number is.
+
+#5.If player's guess is less than the target number say "Oops, your score was LOW"
+#If player's guess is greater than target number say, "Oops. Your score was HIGH"
+
+
+#6.If player's guess is equal to target number: "Good job, [name]! You guessed my
+#number in [number of guesses] guesses!"
+
+#7.If the player runs out of turns without guessing correctly, say, "Sorry. You
+#didn't get my number. My number was [tagrget]"
+
+#8. Keep allowing he player to guess until they get it right or run out of turns. 

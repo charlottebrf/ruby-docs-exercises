4eacfe9cc63831d1ab9597de12529a8e1af09c77
Charlotte <charlotte@Charlottes-MacBook-Air.local>
2016-12-25 20:13:40 +0000

    adds learning summaries from chapter 1

---
+++ learning_chapter1.rb
@@ -0,0 +1,27 @@
+#Ruby is an interpreted language, don't need to compile Ruby code before executing
+
+#don't need to declare variables before assigning them or specify a type
+
+# #comments
+
+#"string"
+
+#if you include a #{...} in a ruby string, expression within the braces interpolated in
+#the string
+
+#method calls may need 1 or mor more arguments, separated by commas
+
+#parenthesis optional around method arguments, leave them off if not passing arguments
+
+#use the inspect and p methods to view debug output for Ruby objects
+
+#can include special characters within double-quoted strings by using escape sequences
+#\n and \t
+
+
+#call to_s on almost any object to convert it to a string.
+#Call to_i on a string to convert to an integer
+
+#unless is the opposite of if  (code won't execute unless a statement is false)
+
+#until is opposite of while (executes repeatedly untul condition is true)

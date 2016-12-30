be17c58e408c78d69fe146d1b4a0008e89ee9aa9
Charlotte Fereday <cbrfereday@gmail.com>
2016-12-30 10:33:51 +0000

    Adds section 2 on Classes

---
+++ head_first_ruby_methods_classes2.rb
@@ -0,0 +1,33 @@
+#Designing a class
+
+#benefit of using objects is they keep a set of data & the methods that operate
+#on that data in one place
+
+#To create an object you're going to need classes
+#Class is a blueprint for making objects
+#When use a class to make an object, the class describes what the object knows
+#about itself,as well as what that object does
+
+#Things an object knows about itself are called: Instance variables
+#variables that belong to one object, comprise of everything the object knows about
+#itself. Represent the object's state (its data) & can have differentt values for each instance
+#of the class
+
+#Things an object does are called: Instance methods
+#methods that you call directly on that object. Comprise of what the object does.
+#Have access to the object's variables & use them to change their behaviour based
+#on the values in those classes
+
+#User knows: name, password -Instance Variables
+#User does: subscribe, login -Instance methods
+
+#Instance of a class is an object made using that class. Only have to write one class
+#but you can make many instances of that class
+
+#Instance is another way of saying object
+
+#If classes are cookie cutters, objects are the cookies they make
+
+#example- dog class
+#Instance variables (state): name, age
+#Instance methods (behaviour): talk, move, report_age

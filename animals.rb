20b55feaaa1221bb97c3496b0bde34fe09bd240b
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-01 12:59:50 +0000

    adds instance variables

--- animals.rb
+++ animals.rb
@@ -55,3 +55,51 @@ blender.blend ("high")
 
 new_string = "Hello!"
 new_float = 4.2
+
+#can use instance variables to store data inside the object
+#one of key benefits of object-orientated programming is that it keeps data & the methods
+#that operate on the data in the same place
+#storing names in the animal objects so don't have to pass too many arguments in our instance method
+
+#local variables live until the method ends - local to the current scope
+#second round of writing the animal method
+
+class Dog
+
+  def make_up_name
+    @name = "Sandy"
+  end
+
+
+  def talk
+    puts "#{@name} says Bark!"
+  end
+end
+
+def move (destination)
+  puts "#{@name} runs to the #{destination}."
+end
+
+def make_up_age
+  @age = 5
+end
+
+def report_age
+  puts "#{@name} is #{@age} years old"
+end
+
+
+
+
+dog = Dog.new
+dog.make_up_name
+dog.talk
+dog.move("yard")
+dog.make_up_age
+dog.report_age
+
+#Instance variables live as long as the instance does - tied to a particular object instance
+#data written to an objects instance variables stays with that object
+
+#my_variable - local variable
+#@my_variable - Instance variable

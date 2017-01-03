1824dec94ce6767378bc1afd98d3b3c06db63a33
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-03 12:42:29 +0000

    Updates exercise on initialize & super

---
+++ initializing_instances_super_3.rb
@@ -0,0 +1,29 @@
+class Boat
+
+  def initialize (name)
+    @name
+  end
+
+class PowerBoat < boat
+
+  def initialize (name, motor_type)
+    super (name)
+    @motor_type = motor_type
+  end
+
+  def info
+    puts "Name: #{@name}"
+    puts "Motor Type: #{@motor_type}"
+  end
+
+end
+
+
+
+#sample code
+boat = PowerBoat.new("Guppy", "outboard")
+boat.info
+
+
+#what we know: boat name, motor type type -instance variables
+#what we do: print name, print boat type  -instance methods

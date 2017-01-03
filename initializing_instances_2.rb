0c3be17ed0e13569c44991c7e2f5289c9a8b8f27
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-03 13:01:10 +0000

    Updates notes on class methods

--- initializing_instances_2.rb
+++ initializing_instances_2.rb
@@ -106,3 +106,34 @@ hourly_employee.print_pay_stub
 
 #There is duplication in the initialize methods- instead of duplication can add the initialize method to the super class
 #previously used super in the print_pay_stub methods now done the same thing with the initialize method. This works because initialize in an instance method just like any other. Any feature that can be used with an ordinary instance method, can also be used with initialize.
+
+
+#factory method
+
+#When you need to make many instances of a class that have a similar data, can often save some repetition by making a factory method to create objects already pre-populated with the needed attribute values
+
+class HourlyEmployee
+
+  def turn_into_cashier
+    self.hourly_wage = 12.75
+    self.hours_per_week = 25
+  end
+
+end
+
+#If we do this we set the hourly_wage & hours_per_week attributes within initialize, then immediately turn around & set them again within turn_into_cashier
+
+#class methods- methods that you can invoke directly on a class, without the need for any instance of that class. Don't have to use a class method as a factory method, but it's perfect for the job.
+
+class Myclass
+  def MyClass.my_class_method(p1, p2)
+    puts "Hello from MyClass!"
+    puts "My parameters: #{p1}, #{p2}"
+  end
+
+end
+
+MyClass.my_class_method(1, 2)
+#can put as many Ruby statements as you like in method body
+#can return a value with return keyword
+#can optionally define one or more parameters that the method accept & can make parameters optional by defining defaults

31071524f7392bd03423c4cf1f93312caccd353a
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-04 15:52:37 +0000

    Adds variables exercises

---
+++ variables_chris_pine.rb
@@ -0,0 +1,22 @@
+#Variables & assignment
+
+myString = "...you can say that again..."
+puts myString
+#variables pointing to data
+
+name = "Patricia Rosanna"
+puts "My name is " + name + " ."
+puts "Wow! " + name + " is an interesting name!"
+
+
+#can assign an object to a variable & then reassign a different object to that variable
+
+composer = "Mozart"
+puts composer + " was great"
+
+composer = "Beethoven"
+puts "But I prefer " + composer + " frankly"
+
+#variables can poinst to any kind of object, not just strings, apart from variables
+var = 5 * (1+2)
+puts var

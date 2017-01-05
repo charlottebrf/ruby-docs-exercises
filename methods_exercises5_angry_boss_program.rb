98540d8be12bb29ebbeb78ffa1e0a633d6cd97ff
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-05 11:32:37 +0000

    Adds angry boss method

--- methods_exercises5_angry_boss_program.rb
+++ methods_exercises5_angry_boss_program.rb
@@ -2,12 +2,14 @@
 #Should rudely ask you what you want
 #whatever you answer- the angry boss should yell back at you & then fire you
 
-def angry_boss
+def angry_boss #(greeting)
   puts "What do you want?!"
-  gets.chomp = response
+  response = gets.chomp
   puts "Whaddya mean you want #{response}?!! You're fired!!"
-end 
+end
 
+#angry_boss("Hello sir")
+#note to self for learning:When to use parameters & call arguments in a method, especially when I'm already defining one method inside of this method.
 
 
 #Write a program that will display a Table of Contents so that it looks to the specification.

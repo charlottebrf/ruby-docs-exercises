22fd0d016df2f3a9beef2ae622fa8329be05a40d
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-05 11:23:10 +0000

    Adds more methods exercises from Ch 5

---
+++ methods_exercises5.rb
@@ -0,0 +1,13 @@
+#Write an angry boss program. Specifications:
+#Should rudely ask you what you want
+#whatever you answer- the angry boss should yell back at you & then fire you
+
+def angry_boss
+  puts "What do you want?!"
+  gets.chomp = response
+  puts "Whaddya mean you want #{response}?!! You're fired!!"
+end 
+
+
+
+#Write a program that will display a Table of Contents so that it looks to the specification.

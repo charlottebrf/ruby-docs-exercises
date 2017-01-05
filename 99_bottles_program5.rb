2e824f936e296d2164d22d6bd9dd05b1d8591977
Charlotte Fereday <cbrfereday@gmail.com>
2017-01-05 18:46:27 +0000

    Adds my if /else statement 99 bottles program & Chris Pine's full solution using while, if & else:

--- 99_bottles_program5.rb
+++ 99_bottles_program5.rb
@@ -1,19 +1,31 @@
 #Write a program that prints lyrics to "99 Bottles of Beer on the Wall"
 
-#def bottle_of_beer(number)
-  number = gets.chomp
-  if number >= 1
-    puts "#{number} bottles of beer on the wall, #{number} bottles of beer"
-  end
-end 
+#Version 1
+def bottle_of_beer (number)
+ puts "To get the lyrics to 99 bottles on the wall type in an integer between 1-99"
+  #number = gets.chomp
+  if (number >= 1 && number <= 99)
+    new_number = number.to_i - 1
+   puts number.to_s + " bottles of beer on the wall, " + number.to_s + " bottles of beer"
+   puts "Take one down and pass it around, " + new_number.to_s + " bottles of beer on the wall."
+  else
+    puts "I'm sorry you can only have a number between 1-99 :-/"
+ end
+end
+
+bottle_of_beer(8)
+
+#Note for learning: work out why gets.chomp wouldn't work & why the while loop wouldn't work either
+#Failed version 2- using while loop!
 
 #bottle_of_beer(0)
-
-#while bottle_of_beer >= 1 && bottle_of_beer <= 99
-  #this is wrong -need to work out what's specifcally wrong here
-#  puts "#{bottle_of_beer} bottles of beer on the wall, #{bottle_of_beer} bottles of beer"
-#  bottle_of_beer = gets.chomp
+#number = 0
+#while (number >= 1 && number <= 99)
+  #number = gets.chomp
+#puts number.to_s + " bottles of beer on the wall, " + number.to_s + "bottles of beer"
 #end
+
+#bottle_of_beer(8)
 
 #new_bottle_of_beer = bottle_of_beer - 1
 
@@ -23,9 +35,30 @@ end
 
 #bottle_of_beer(99)
 
-#99 bottles of beer on the wall, 99 bottles of beer.
-#print the bottle_of_beer number of beers on the wall
-#Take one down and pass it around, 98 bottles of beer on the wall.
-#need to take that bottle_of_beer - 1 & print new_bottle_of_beer
-#need to do this while numbers are >= 1 && <= 99
-#else print: No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.
+#Version 3- Chris Pine's full solution of the song from 1-99
+#Note to self improve on while, if & else !
+
+bottles = 99
+while bottles != 0
+puts bottles.to_s + ' bottles of beer on the wall'
+puts bottles.to_s + ' bottles of beer'
+bottles = bottles - 1
+puts 'take one down, pass it around'
+
+if bottles == 1
+puts bottles.to_s + ' bottle of beer on the wall'
+else
+puts bottles.to_s + ' bottles of beer on the wall'
+end
+
+puts ''
+
+if bottles == 1
+puts bottles.to_s + ' bottle of beer on the wall'
+puts bottles.to_s + ' bottle of beer'
+bottles = bottles - 1
+puts 'take one down, pass it around'
+puts bottles.to_s + ' bottles of beer on the wall'
+end
+
+end
